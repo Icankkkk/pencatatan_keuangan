cdd9eba7b8bed8297f5a16e49ca93c96ecea8185
Muhamad Faisal <muhamadfaisal280201@gmail.com>
2023-07-12 21:40:32 +0700

    setup app request

--- lib/data/controller_user.dart
+++ lib/data/controller_user.dart
@@ -1,5 +1,8 @@
 import 'package:get/get.dart';
+import 'package:pencatatan_keuangan/data/model/user.dart';
 
-Class ControllerUser  {
-
+class ControllerUser extends GetxController {
+  final _data = User().obs;
+  User get data => _data.value;
+  setData(dynamic n) => _data.value = n;
 }
