c2a01f3a682b69b06ed3bdd35dad1a6f55200ecf
Muhamad Faisal <muhamadfaisal280201@gmail.com>
2023-07-28 08:53:29 +0700

    Home Page - UI Create Header

--- lib/presentation/page/home_page.dart
+++ lib/presentation/page/home_page.dart
@@ -1,27 +1,82 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:pencatatan_keuangan/config/session.dart';
-import 'package:pencatatan_keuangan/presentation/page/login_page.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:pencatatan_keuangan/config/app_asset.dart';
+import 'package:pencatatan_keuangan/config/app_color.dart';
+import 'package:pencatatan_keuangan/data/controller_user.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final controllerUser = Get.put(ControllerUser());
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text('Home Page'),
-            IconButton(
-              onPressed: () {
-                Session.clearUser();
-                Get.off(() => const LoginPage());
-              },
-              icon: const Icon(Icons.logout),
-            ),
-          ],
+      endDrawer: const Drawer(),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(20, 10, 20, 30),
+          child: Column(
+            children: [
+              _buildHeader(),
+              Expanded(child: ListView()),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildHeader() {
+    return Row(
+      children: [
+        Image.asset(AppAsset.profile),
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Hi,',
+                style: GoogleFonts.poppins(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
+                ),
+              ),
+              Obx(
+                () => Text(
+                  controllerUser.data.name ?? '',
+                  style: GoogleFonts.poppins(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        _buildMenuButton(context),
+      ],
+    );
+  }
+
+  Widget _buildMenuButton(BuildContext context) {
+    return Material(
+      borderRadius: BorderRadius.circular(4),
+      color: AppColor.lev3,
+      child: InkWell(
+        onTap: () => Scaffold.of(context).openEndDrawer(),
+        child: const Padding(
+          padding: EdgeInsets.all(10),
+          child: Icon(
+            Icons.menu,
+            color: AppColor.lev1,
+          ),
         ),
       ),
     );
