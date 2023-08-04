0eeba89f4111dc49897f2f632ceda8d5d4fe97ee
Muhamad Faisal <muhamadfaisal280201@gmail.com>
2023-08-05 03:45:04 +0700

    Update Home Page
    - create home_widget.dart
    - Extract methods and move to home_widget
    - update perbandingan bulan ini (pie chart)

--- lib/presentation/controller/controller_home.dart
+++ lib/presentation/controller/controller_home.dart
@@ -10,10 +10,10 @@ class HomeController extends GetxController {
   String get todayPercent => _todayPercent.value;
 
   final _week = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0].obs;
+  // ignore: invalid_use_of_protected_member
   List<double> get week => _week.value;
 
   List<String> get days => ['Sen', 'Sel', 'Rab', 'Kam', 'Jum', 'Sab', 'Min'];
-
   List<String> weekText() {
     DateTime today = DateTime.now();
     return [
@@ -27,10 +27,25 @@ class HomeController extends GetxController {
     ];
   }
 
+  final _monthIncome = 0.0.obs;
+  double get monthIncome => _monthIncome.value;
+
+  final _monthOutcome = 0.0.obs;
+  double get monthOutcome => _monthOutcome.value;
+
+  final _percentIncome = '0'.obs;
+  String get percentIncome => _percentIncome.value;
+
+  final _monthPercent = ''.obs;
+  String get monthPercent => _monthPercent.value;
+
+  final _differentMonth = 0.0.obs;
+  double get differentMonth => _differentMonth.value;
+
   getAnalysis(String idUser) async {
     Map data = await SourceHistory.analysis(idUser);
 
-    // Today outcome
+// Today outcome
     _today.value = data['today'].toDouble();
     double yesterday = data['yesterday'].toDouble();
     double different = (today - yesterday).abs();
@@ -45,5 +60,20 @@ class HomeController extends GetxController {
             : '-${percent.toStringAsFixed(1)}% dibanding kemarin';
 
     _week.value = List.castFrom(data['week'].map((e) => e.toDouble()).toList());
+
+    _monthIncome.value = data['month']['income'].toDouble();
+    _monthOutcome.value = data['month']['outcome'].toDouble();
+    _differentMonth.value = (monthIncome - monthOutcome).abs();
+    bool isSameMonth = monthIncome.isEqual(monthOutcome);
+    bool isPlusMonth = monthIncome.isGreaterThan(monthOutcome);
+    double dividerMonth =
+        (monthIncome + monthOutcome) == 0 ? 1 : (monthIncome + monthOutcome);
+    double percentMonth = (differentMonth / dividerMonth) * 100;
+    _percentIncome.value = percentMonth.toStringAsFixed(1);
+    _monthPercent.value = isSameMonth
+        ? 'Pemasukan\n100% sama dengan \nPengeluaran'
+        : isPlusMonth
+            ? 'Pemasukan\nlebih besar $percentIncome%\ndari Pengeluaran'
+            : 'Pemasukan\nlebih kecil $percentIncome%\ndari Pengeluaran';
   }
 }
